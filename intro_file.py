8bb427d246a6ae536c7a029f84e8185a258774d4
dcramer148 <77908264+dcramer148@users.noreply.github.com>
2025-06-20 16:18:59 -0300

    building out files

--- intro_file.py
+++ intro_file.py
@@ -1,1 +1,16 @@
 #Intro file for FF-Wrapped
+
+#SHould be able sleeper API to get lots of different information
+import requests
+
+
+#different parts of this project as i see them:
+#defining a "statistic"
+#gathering the data that we need for that statistic
+#computing the statistic using data from every team
+#UI to represent the results in some way
+#
+#I want it to be such that you can just add a new statistic later without having to rework anything
+
+#league id: 1180280584113598464
+
